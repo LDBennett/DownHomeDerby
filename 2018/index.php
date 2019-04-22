4f91c52b2cd08b01312e6e6cb73cf0883e02ce0c
Lee Bennett <LDBennett@gmail.com>
2019-04-22 17:01:04 -0400

    Added and separated 2018 and 2019s website

---
+++ 2018/index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+	<title>Down Home Derby | Habitat for Humanity - Allegheny Valley</title>
+	<meta name="description" content="Down Home Derby - Celebrate the Run for the Roses with Southern style appetizers, cash bar, friendly “wagers” and a silent auction - all to benefit Habitat for Humanity Allegheny Valley. Ladies may wear their finest hats and gents their finest bow ties. ">
+	<meta name="keywords" content="Down Home Derby, Habitat for Humanity, Allegheny Valley">
+	<meta name="author" content="Lee Bennett">
+
+	<!--**********FAVICONS************-->
+	<link rel='apple-touch-icon' sizes='57x57' href='img/f/apple-icon-57x57.png'>
+	<link rel='apple-touch-icon' sizes='60x60' href='img/f/apple-icon-60x60.png'>
+	<link rel='apple-touch-icon' sizes='72x72' href='img/f/apple-icon-72x72.png'>
+	<link rel='apple-touch-icon' sizes='76x76' href='img/f/apple-icon-76x76.png'>
+	<link rel='apple-touch-icon' sizes='114x114' href='img/f/apple-icon-114x114.png'>
+	<link rel='apple-touch-icon' sizes='120x120' href='img/f/apple-icon-120x120.png'>
+	<link rel='apple-touch-icon' sizes='144x144' href='img/f/apple-icon-144x144.png'>
+	<link rel='apple-touch-icon' sizes='152x152' href='img/f/apple-icon-152x152.png'>
+	<link rel='apple-touch-icon' sizes='180x180' href='img/f/apple-icon-180x180.png'>
+	<link rel='icon' type='image/png' sizes='192x192' href='img/f/android-icon-192x192.png'>
+	<link rel='icon' type='image/png' sizes='32x32' href='img/f/favicon-32x32.png'>
+	<link rel='icon' type='image/png' sizes='96x96' href='img/f/favicon-96x96.png'>
+	<link rel='icon' type='image/png' sizes='16x16' href='img/f/favicon-16x16.png'>
+	<meta name="msapplication-TileColor" content="#00611a">
+	<meta name="msapplication-TileImage" content="img/f/ms-icon-144x144.png">
+	<meta name="theme-color" content="#4fad25">
+
+	<!--Import Fonts-->
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Dancing+Script|Noto+Sans" rel="stylesheet">
+
+	<!-- Compiled and minified CSS -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+
+	<!--Custom CSS-->
+	<link rel="stylesheet" href="css/animate.css">
+	<link rel="stylesheet" href="css/main.css">
+
+</head>
+
+<body style="height:100%">
+	<nav>
+		<div class="nav-wrapper">
+			<a href="http://www.habitatav.org" class="brand-logo center" target="_blank"><img src="img/logo_header.png" alt="Habitat for Humnaity Allegheny Valley Logo" id="h-img"></a>
+		</div>
+	</nav>
+	<header>
+		<div id="sun" class="animated bounceInDown">
+			<img src="img/header-bg.jpg" alt="Sun portion of header" class="responsive-img" style="height:100%; width:100%;">
+			<h1>Down <span style="color:red; text-shadow: 2px 2px black;">Home</span> Derby</h1>
+			<p>Saturday, May 5, 2018<br>4:30 p.m. - 8:00 p.m.<br><span style="color:#826f51;">Derby Post Time - 6:46 p.m.</span></p>
+		</div>
+		<div id="grass" class="animated bounceInUp">
+			<div id="horses" class="" style="display:none;">
+				<img class="floating" src="img/horse.png" alt="">
+				<img class="floating2" src="img/horse.png" alt="">
+				<img class="floating3" src="img/horse.png" alt="">
+			</div>
+		</div>
+	</header>
+	<main>
+
+	</main>
+	<!--Materialize JS-->
+	<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+
+	<script>
+		setTimeout(function() {
+				$('#horses').fadeIn().addClass('animated fadeInLeft')
+			},
+			1000);
+
+	</script>
+</body>
+
+</html>
